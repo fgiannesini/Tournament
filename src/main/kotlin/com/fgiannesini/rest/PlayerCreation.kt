45654362fcf8adeceb15c0d4fe04f36cb4f22e17
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-02-11 11:02:01 +0100

    Remove unused code

--- src/main/kotlin/com/fgiannesini/rest/PlayerCreation.kt
+++ src/main/kotlin/com/fgiannesini/rest/PlayerCreation.kt
@@ -1,12 +1,6 @@
 package com.fgiannesini.rest
 
-import com.fgiannesini.domain.NO_ID
-import com.fgiannesini.domain.Player
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PlayerCreation(val pseudo : String) {
-    fun toPlayer(): Player {
-        return Player(NO_ID, pseudo, 0)
-    }
-}
+data class PlayerCreation(val pseudo: String)
